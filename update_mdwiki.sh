f9b7c210e1b594edcf82479780bd34244b22dd00
Jan Baer <jan@janbaer.de>
2017-12-02 17:27:03 +0100

    Change update_mdwiki.sh to restart only the mdwiki container after pulling the newest image

--- update_mdwiki.sh
+++ update_mdwiki.sh
@@ -2,10 +2,14 @@ export HOME="/root"
 
 cd "${HOME}/mdwiki"
 
-docker pull janbaer/mdwiki:latest > update_mdwiki.log
+echo "Start updating MDWiki at $(date)" > update_mdwiki.log
+
+docker pull janbaer/mdwiki:latest >> update_mdwiki.log
 
 echo "Latest version pulled" >> update_mdwiki.log
 
-docker-compose down &>> update_mdwiki.log
+docker stop mdwiki_mdwiki_1 >> update_mdwiki.log
 
-./start.local.sh
+docker start mdwiki_mdwiki_1 >> update_mdwiki.log
+
+echo "Finished with updating MDWiki at $(date)" >> update_mdwiki.log
