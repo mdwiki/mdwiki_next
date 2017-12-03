4ba4288a9970fd62eb74ecc36c9bdb86d3ec5930
Jan Baer <jan@janbaer.de>
2017-12-03 10:06:08 +0100

    Change updating for Docker since docker start and stop does not use the new image
    - Read now the sensible environment variable from an external and only
    locally env file. So the credentials will not be saved in Github

--- update_mdwiki.sh
+++ update_mdwiki.sh
@@ -8,8 +8,8 @@ docker pull janbaer/mdwiki:latest >> update_mdwiki.log
 
 echo "Latest version pulled" >> update_mdwiki.log
 
-docker stop mdwiki_mdwiki_1 >> update_mdwiki.log
+docker-compose down >> update_mdwiki.log
 
-docker start mdwiki_mdwiki_1 >> update_mdwiki.log
+docker-compose up -d &>> update_mdwiki.log
 
 echo "Finished with updating MDWiki at $(date)" >> update_mdwiki.log
