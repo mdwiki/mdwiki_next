3f7c81a1edd4990eea9f70543f5ba6a1f05341c5
Jan Baer <jan@janbaer.de>
2018-01-20 18:32:03 +0100

    Improve service-worker implementation

--- post-build.js
+++ post-build.js
@@ -1,17 +1,11 @@
 const fs = require('fs');
-const buildStats = require('./.next/build-stats.json');
+const appVersion = require('./package.json').version;
 
-const BUILD_ID_FILEPATH = `${__dirname}/.next/BUILD_ID`;
 const SERVICEWORKER_FILEPATH = `${__dirname}/server/service-worker.js`;
-
-const buildId = fs.readFileSync(BUILD_ID_FILEPATH, { encoding: 'utf8' });
-const appJSHash = buildStats['app.js'].hash;
 
 let serviceWorkerCode = fs.readFileSync(SERVICEWORKER_FILEPATH, { encoding: 'utf8' });
 
-serviceWorkerCode = serviceWorkerCode.replace(/BUILD_ID/g, buildId);
-serviceWorkerCode = serviceWorkerCode.replace(/APPJS_HASH/g, appJSHash);
-serviceWorkerCode = serviceWorkerCode.replace(/NODE_ENV/g, process.env.NODE_ENV);
+serviceWorkerCode = serviceWorkerCode.replace(/1\.0\.0/g, appVersion);
 
 fs.writeFileSync(SERVICEWORKER_FILEPATH, serviceWorkerCode, { encoding: 'utf8' });
 
--- server/service-worker.js
+++ server/service-worker.js
@@ -1,44 +1,127 @@
 /* eslint-env worker, serviceworker */
 /* eslint no-restricted-globals: ["off", "self"] */
 
-const buildId = 'BUILD_ID';
-const appJSHash = 'APPJS_HASH';
-const ENV = 'NODE_ENV';
+const APP_VERSION = '1.0.0';
 
-const APP_CACHE_NAME = 'mdwiki-cache-v2';
-const GITHUB_CACHE_NAME = 'mdwiki-github-cache';
-const HOST_ADDRESS = 'https://www.mdwiki.net';
+const EXTERNALS_CACHE_NAME = 'mdwiki-externals-cache-v1';
+const APP_CACHE_NAME = `mdwiki-app-cache-v${APP_VERSION}`;
+const API_CACHE_NAME = 'mdwiki-api-cache';
 
-const filesToPrecache = [
-  './index',
-  `./_next/${buildId}/page/index.js`,
-  `./_next/${buildId}/page/_error/index.js`,
-  `./_next/${appJSHash}/app.js`,
-  './static/manifest.json',
-  './static/scripts/register-service-worker.js',
-  './service-worker.js',
-  './static/scripts/sw-toolbox.js',
-  './static/styles/simplemde.min.css',
-  './static/styles/markdown.css',
-  './static/styles/styles.css',
-  './static/styles/normalize.css',
-  './static/images/wiki.png',
-  './static/images/favicon.ico',
+const externalLibraries = [
   'https://fonts.googleapis.com/css?family=Roboto:300,400,500',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://fonts.gstatic.com/s/roboto/v18/CWB0XYA8bzo0kSThX0UTuA.woff2',
   'https://fonts.gstatic.com/s/roboto/v18/RxZJdnzeo3R5zSexge8UUVtXRa8TVwTICgirnJhmVJw.woff2'
 ];
 
-if (ENV === 'production') {
-  importScripts('./../static/scripts/sw-toolbox.js');
+const cacheRules = [
+  /^\/$/,
+  /^\/index$/,
+  /^\/connect$/,
+  /^\/search$/,
+  /^\/_next\//,
+  /^\/static\//,
+  /^\/index\//
+];
 
-  self.toolbox.options.cache.name = APP_CACHE_NAME;
-  self.toolbox.precache(filesToPrecache);
-  self.toolbox.router.get(`${HOST_ADDRESS}/(.*)`, self.toolbox.cacheFirst);
+const apiCacheRule = /^\/api\//;
 
-  self.toolbox.router.get(/^https:\/\/www.mdwiki.net\/api\//, self.toolbox.networkFirst, {
-    cache: { name: GITHUB_CACHE_NAME }
-  });
+self.addEventListener('install', event => {
+  const precachePromise = addToCache(EXTERNALS_CACHE_NAME, externalLibraries);
+  event.waitUntil(precachePromise);
+  console.log('ServiceWorker successful installed');
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(cleanOldCaches());
+  console.log('ServiceWorker successfully activated');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(handleFetch(event.request));
+});
+
+async function handleFetch(request) {
+  const requestUrl = new URL(request.url);
+
+  if (isExternalRequest(requestUrl)) {
+    return cacheFirst(EXTERNALS_CACHE_NAME, request);
+  }
+
+  if (shouldRequestBeCached(requestUrl)) {
+    return cacheFirst(APP_CACHE_NAME, request, requestUrl.pathname);
+  }
+
+  if (isApiRequest(requestUrl)) {
+    if (!navigator.onLine) {
+      return cacheFirst(API_CACHE_NAME, request, requestUrl.pathname);
+    }
+    return networkFirst(API_CACHE_NAME, requestUrl, requestUrl.pathname);
+  }
+
+  console.log('Fetch without cache', requestUrl.origin, requestUrl.pathname);
+  return fetch(request);
 }
 
+async function isApiRequest(requestUrl) {
+  return apiCacheRule.exec(requestUrl.pathname) !== null;
+}
+
+function shouldRequestBeCached(requestUrl) {
+  let shouldBeCached = false;
+
+  for (const cacheRule of cacheRules) {
+    shouldBeCached = cacheRule.exec(requestUrl.pathname) !== null;
+    if (shouldBeCached) {
+      break;
+    }
+  }
+
+  return shouldBeCached;
+}
+
+function isExternalRequest(requestUrl) {
+  return requestUrl.origin !== location.origin;
+}
+
+
+async function addToCache(cacheName, filesToCache) {
+  const cache = await caches.open(cacheName);
+  return cache.addAll(filesToCache);
+}
+
+async function cacheFirst(cacheName, request, cacheKey = request) {
+  const cache = await caches.open(cacheName);
+
+  let response = await cache.match(cacheKey);
+  if (!response) {
+    response = await fetch(request);
+    cache.put(cacheKey, response.clone());
+  }
+
+  return response;
+}
+
+async function networkFirst(cacheName, request, cacheKey) {
+  const cache = await caches.open(cacheName);
+
+  return fetch(request)
+    .then(response => {
+      cache.put(cacheKey, response.clone());
+      return response;
+    })
+    .catch(error => cache.match(cacheKey));
+}
+
+async function cleanOldCaches() {
+  const activeCacheKeys = [EXTERNALS_CACHE_NAME, APP_CACHE_NAME, API_CACHE_NAME];
+
+  const existingCacheKeys = await caches.keys();
+
+  const cachesToDelete = existingCacheKeys.filter(key => !activeCacheKeys.includes(key));
+  if (cachesToDelete.length > 0) {
+    console.log('Delete the following old caches', cachesToDelete);
+    return Promise.all(cachesToDelete.map(key => caches.delete(key)));
+  }
+  return undefined;
+}
