a0c7b22f97950801f8415d845431b448dc64eae0
Jan Baer <jan@janbaer.de>
2018-02-24 15:05:34 +0100

    Fix the problem with the browser history navigation
    When the user was navigation back then page wasn't updated correctly
    
    Closes #13

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { observer } from 'mobx-react';
 import PageLayout from './../components/page-layout.js';
 import Page from './../components/page.js';
 import navigator from './../services/navigator.service.js';
+import appStore from './../stores/app.store.js';
 
 @observer export default class IndexPage extends React.Component {
   static async getInitialProps({ query }) {
@@ -14,6 +15,12 @@ import navigator from './../services/navigator.service.js';
   componentDidMount() {
     const pageName = this.props.pageName || 'index';
     navigator.gotoPage(pageName);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (appStore.selectedPage !== nextProps.pageName) {
+      navigator.gotoPage(nextProps.pageName);
+    }
   }
 
   render() {
